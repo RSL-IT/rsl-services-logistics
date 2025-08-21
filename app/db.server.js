7d492df09997d6b18a659b2b2f6135b280469cb9
RSL-IT <itadmin@rslspeakers.com>
2025-08-21 17:01:40 -0500

    Setting up PRISMA schema from RSL RETURNS spreadsheet.

--- app/db.server.js
+++ app/db.server.js
@@ -1,11 +1,13 @@
-import { PrismaClient } from "@prisma/client";
+//import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+
 
 if (process.env.NODE_ENV !== "production") {
   if (!global.prismaGlobal) {
     global.prismaGlobal = new PrismaClient();
   }
 }
-
-const prisma = global.prismaGlobal ?? new PrismaClient();
+const prisma = new PrismaClient().$extends(withAccelerate())
 
 export default prisma;
